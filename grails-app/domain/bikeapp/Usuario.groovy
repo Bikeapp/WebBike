54c179cc36e45e3f2ffbc3f2fca263d61c6524c4
Daniel Moncada <ndmoncadam@gmail.com>
2016-03-03 20:38:26 -0500

    domain/bikeapp/usuario.groovy

--- grails-app/domain/bikeapp/Usuario.groovy
+++ grails-app/domain/bikeapp/Usuario.groovy
@@ -4,13 +4,24 @@ import groovy.transform.ToString
 @ToString
 class Usuario {
 
-	static hasMany = [puntos: PuntoInteres]
+	static hasMany = [puntos: PuntoInteres, fotos: Foto]
 
-	String usuario
-	String email
-	String contrasena
-	String fecha
+	String usuario		//Nombre de usuario para login
+		String email		//Email del usuario
+		String contraseña	//Contraseña para login
+		String fecha		//Fecha nacimiento
+		String interes		//Lista de intereses
+		String nombre		//Nombre real del usuario
+		String sexo			//Sexo del usuario
+		String foto			//Foto de perfil
+		String mensaje		//Mensaje dinstintivo del usuario
+		String rol			// ???? No se para que lo usamos la verdad....
 
     static constraints = {
+		usuario(nullable:false)
+		email(nullable:false)
+		contraseña(nullable:false)
+		fecha(nullable:false)
+		interes(nullable:false)
     }
 }
